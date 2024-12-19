3f21ca50c17e0ea27de684601c56e8eaaac49012
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-12-19 18:27:35 +0100

    #1: fix unit tests names

--- tests/unit/foo/bar/bar_test.cc
+++ tests/unit/foo/bar/bar_test.cc
@@ -3,7 +3,7 @@
 #include "foo/bar/bar.h"
 
 // Demonstrate some basic assertions.
-TEST(BarTest, BasicAssertions) {
+TEST(FooUnitTests, TestBarDiff) {
   auto bar = foo::bar::Bar();
   auto n = bar.diff(5, 3);
   EXPECT_EQ(n, 2);
--- tests/unit/foo/foo_test.cc
+++ tests/unit/foo/foo_test.cc
@@ -3,7 +3,7 @@
 #include "foo/foo.h"
 
 // Demonstrate some basic assertions.
-TEST(FooTest, BasicAssertions) {
+TEST(FooUnitTests, TestFooSum) {
   auto foo = foo::Foo();
   auto n = foo.sum(5, 3);
   EXPECT_EQ(n, 8);
